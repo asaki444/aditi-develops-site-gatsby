6923d2cd5d89a3299b0b5985bef4a58b55678f96
Aditi <asaki444@gmail.com>
2020-06-02 23:28:05 -0400

    added blog template to portfolio page

--- aditi-port/src/pages/portfolio.js
+++ aditi-port/src/pages/portfolio.js
@@ -1,10 +1,11 @@
 import React from 'React';
 import Layout from '../components/Layout';
+import Blog from '.../templates/blog.js'
 
 const Portfolio = () => {
    return (
     <Layout>
-
+       <Blog />
     </Layout>
    )
 }
